74321d4118ddcf95f8da60aaa181104351814a34
OpenShift guest <omt66@yahoo.com>
2013-02-14 11:34:03 -0500

    From Cloud9 (14 Feb 2013/Thr)

--- misc.cpp
+++ misc.cpp
@@ -12,5 +12,11 @@ int main(int argc, char *argv[]) {
 	// Another line (18 Aug 2011/Thr)
 	cout << "Yet another line now!" << endl;
 	
+    for (int i=0;i < 10;i++) {
+        cout << "i-> " << i << endl;
+    }
+    
+    // Test by OT
+    
 	return 0;
 }
