0321512f268b1fbeae7c2e1b97e20b9e2c0a5e50
omt66 <omt66@yahoo.com>
2013-03-28 17:27:09 -0700

    Update misc.cpp

--- misc.cpp
+++ misc.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     cout << "Bye bye now :)" << endl;
 	
     // Another line (18 Aug 2011/Thr)
-	cout << "Yet another line now!" << endl;
+    cout << "Yet another line now!" << endl;
 	
     for (int i=0;i < 10;i++) {
         cout << "i-> " << i << endl;
